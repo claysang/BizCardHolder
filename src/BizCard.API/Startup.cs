6dc8a1acad757c8462f4b0f9b12a4493cf3426b9
Clay Sang <cs@claysang.com>
2020-04-07 20:44:56 +0800

    Update MVC compatibility to Version_3_0

--- src/BizCard.API/Startup.cs
+++ src/BizCard.API/Startup.cs
@@ -27,7 +27,7 @@ namespace BizCard.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             services.AddDataServices(Configuration);
         }
