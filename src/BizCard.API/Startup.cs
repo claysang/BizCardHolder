e74614bd20dccbf04f193d2164e14a30c0679cd9
Clay Sang <cs@claysang.com>
2020-04-13 10:52:49 +0800

    Use IWebHostEnvironment instead of deprecated IHostingEnvironment

--- src/BizCard.API/Startup.cs
+++ src/BizCard.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -33,7 +34,7 @@ namespace BizCard.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
