573dc28047f9c90dcb5381dc40e42174f09a8c50
Clay Sang <cs@claysang.com>
2020-04-13 11:35:57 +0800

    Use PostgreSQL as database

--- src/BizCard.Core/Models/Entity.cs
+++ src/BizCard.Core/Models/Entity.cs
@@ -8,7 +8,6 @@ namespace BizCard.Core.Models
     {
         public static readonly DateTime EntityInitialDate = new DateTime(2002, 2, 13, 0, 0, 0, DateTimeKind.Utc);
         
-        
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
