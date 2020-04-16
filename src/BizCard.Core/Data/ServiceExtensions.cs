6e54dd15f626a9b82e43c79ba6385e870bb4e4d4
Clay Sang <cs@claysang.com>
2020-04-16 16:29:36 +0800

    Refine logic for EnsureDatabase for PostgreSQL

--- src/BizCard.Core/Data/ServiceExtensions.cs
+++ src/BizCard.Core/Data/ServiceExtensions.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Npgsql;
 
 namespace BizCard.Core.Data
 {
     public static class ServiceExtensions
     {
-        public const string ConfigKeyNpgsqlConnectionString = "npgsqlConnectionString";
+        private const string ConfigKeyNpgsqlConnectionString = "npgsqlConnectionString";
 
         public static void AddDataServices(this IServiceCollection services, IConfiguration appConfiguration)
         {
@@ -25,19 +23,12 @@ namespace BizCard.Core.Data
         public static void EnsureDatabase(this IApplicationBuilder app, Action<string> databaseInitializer)
         {
             var services = app.ApplicationServices;
-
             var appConfiguration = services.GetService<IConfiguration>();
-
             var connStr = appConfiguration.GetConnectionString(ConfigKeyNpgsqlConnectionString);
 
-            var dataSource = new NpgsqlConnection().DataSource;
-
-            if (!File.Exists(dataSource))
-            {
-                services.GetService<IApplicationLifetime>()
-                    .ApplicationStarted
-                    .Register(() => databaseInitializer(connStr));
-            }
+            services.GetService<IApplicationLifetime>()
+                .ApplicationStarted
+                .Register(() => databaseInitializer(connStr));
         }
     }
 }
