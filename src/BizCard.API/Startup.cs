80f4892a725de4a77b5c8f2e08bd4a0ab104f43d
Clay Sang <cs@claysang.com>
2020-04-16 16:32:52 +0800

    Remove no-op SetCompatibilityVersion method for .NET Core 3.0+

--- src/BizCard.API/Startup.cs
+++ src/BizCard.API/Startup.cs
@@ -21,7 +21,7 @@ namespace BizCard.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc();
 
             services.AddDataServices(Configuration);
         }
